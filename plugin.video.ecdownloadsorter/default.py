88a9349e8cada337b07767d325db832ee2583182
mikaelec <mikaelec@gmail.com>
2014-04-18 12:28:48 +0200

    Lots of speedup, no new features added

--- plugin.video.ecdownloadsorter/default.py
+++ plugin.video.ecdownloadsorter/default.py
@@ -1,77 +1,66 @@
-import xbmc
-import xbmcaddon
-import xbmcgui
-import xbmcplugin
-import os
 import sys
 import urlparse
+import xbmcgui
 
-#Addon info
-ADDON = xbmcaddon.Addon()
-NAME = ADDON.getAddonInfo("name")
-ICON = ADDON.getAddonInfo("icon")
-PATH = ADDON.getAddonInfo("path")
-sys.path.append(xbmc.translatePath(os.path.join(PATH, "resources", "lib" )))
-DIALOG = xbmcgui.Dialog()
+PARAMS = urlparse.parse_qs(sys.argv[2][1:])
+types = {"movie": 0, "tv show": 1, "sports": 2, "ignore": 3}
 
-#Imports after path append
+if "index" in PARAMS:
+    isUpdateMode = True
+    ret = xbmcgui.Dialog().select("Choose type", sorted(types, key = types.get))
+else:
+    isUpdateMode = False
+    ret = -1
+
+import xbmc
+import xbmcaddon
+import xbmcplugin
+import os
 from ecftp import ecftp
 from ecdb import ecdb
 
 #Addon state
+ADDON = xbmcaddon.Addon()
 PATH = sys.argv[0]
 HANDLE = int(sys.argv[1])
-PARAMS = urlparse.parse_qs(sys.argv[2][1:])
-
-if ADDON.getSetting("path") == "":
-    ADDON.openSettings()
 
 path = ADDON.getSetting("path")
 username = ADDON.getSetting("username")
 password = ADDON.getSetting("password")
 downloadDir = "download"
-types = {"movie": 0, "tv show": 1, "sports": 2, "ignore": 3}
 
-dbpath = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')), "settings.db")
-
-if "index" in PARAMS:
-    ret = DIALOG.select("Choose type", sorted(types, key = types.get))
-
-    index = int(PARAMS["index"][0])
-
-    ftp = ecftp(path, username, password, downloadDir)
-    ftp.login()
-
-    downloads = ftp.listContent()
-
-    file = downloads[index]
-    
-    if ret == types["movie"]:
-        ftp.move(file, ["media", "movies"])
-        xbmc.executebuiltin("UpdateLibrary(video,nfs://%s/volume2/%s/%s/)" % (path, "media/movies", file) )
-        ftp.logout()
-    if ret == types["tv show"]:
-        pass #Not implemented
-    if ret == types["sports"]:
-        ftp.move(file, ["media", "sport"])
-        ftp.logout()
-    if ret == types["ignore"]:
-        db = ecdb(dbpath)
-        db.addIgnored(file)
-        db.close()
+dbpath = os.path.join(xbmc.translatePath(ADDON.getAddonInfo("profile")), "settings.db")
 
 ftp = ecftp(path, username, password, downloadDir)
 ftp.login()
 
+db = ecdb(dbpath)
+
 downloads = ftp.listContent()
 
-db = ecdb(dbpath)
-db.createTable()
-db.cleanIgnored(downloads)
+if ret != -1:
+    index = int(PARAMS["index"][0])
+    file = downloads.pop(index)
+    
+    if ret == types["movie"]:
+        ftp.move(file, ["media", "movies"])
+        xbmc.executebuiltin("UpdateLibrary(video,nfs://%s/volume2/%s/%s/)" % (path, "media/movies", file) )
+    elif ret == types["tv show"]:
+        pass #Not implemented
+    elif ret == types["sports"]:
+        ftp.move(file, ["media", "sport"])
+    elif ret == types["ignore"]:
+        db = ecdb(dbpath)
+        db.addIgnored(file)
+
+if not isUpdateMode:
+    db.createTable()
+    db.cleanIgnored(downloads)
 
 ignored = db.getIgnored()
+downloadsSize = len(downloads)
 
-for i in range(0, len(downloads)):
+for i in range(0, downloadsSize):
     isIgnored = False
 
     for k in ignored:
@@ -80,10 +69,9 @@ for i in range(0, len(downloads)):
     
     if not isIgnored:
         listitem = xbmcgui.ListItem(downloads[i])
-        xbmcplugin.addDirectoryItem(HANDLE, PATH + "?index=%d" % i, listitem, isFolder=True, totalItems=len(downloads))
-
-xbmcplugin.endOfDirectory(HANDLE, updateListing=True)
-
+        xbmcplugin.addDirectoryItem(HANDLE, PATH + "?index=%d" % i, listitem, isFolder=True, totalItems=downloadsSize)
+        
 db.close()
-
 ftp.logout()
+
+xbmcplugin.endOfDirectory(HANDLE, updateListing=isUpdateMode)
